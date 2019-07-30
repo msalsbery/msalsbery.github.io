1272f860986d597bc79ad803592146090bdd28c3
Mark Salsbery <msalsbery@hotmail.com>
2019-07-30 11:57:04 -0700

    Added OpenSeadragon Imaging Tester client app/site

---
+++ openseadragon-imaging/tester/lib/openseadragon-imaging/openseadragon-consolehook.js
@@ -0,0 +1,121 @@
+//! openseadragon-consolehook 2.0.0
+//! Build date: 2019-06-29
+//! Git commit: --24355e6-dirty
+//! https://github.com/msalsbery/OpenSeadragonConsoleHook
+/*
+ * Copyright (c) 2019 Mark Salsbery
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+/* global OpenSeadragon */
+
+/**
+ * @file
+ * @version  openseadragon-consolehook 2.0.0
+ * @author Mark Salsbery <msalsbery@hotmail.com>
+ *
+ */
+
+/**
+ * @module openseadragon-consolehook
+ * @version  openseadragon-consolehook 2.0.0
+ *
+ */
+
+
+(function (OSD, $, undefined) {
+
+	// if (!OSD.version || OSD.version.major < 1) {
+	// 	throw new Error(
+	// 		'OpenSeadragonConsoleHook requires OpenSeadragon version 1.0.0+'
+	// 	);
+	// }
+
+	/**
+	 * Creates a new ConsoleHook.
+	 *
+	 * @method addConsoleHook
+	 * @memberof external:"OpenSeadragon.Viewer"#
+	 * @param {Object} options
+     * @param {Function} [options.log] - console.log function. Function can return true to prevent original function call.
+     * @param {Function} [options.debug] - console.debug function. Function can return true to prevent original function call.
+     * @param {Function} [options.info] - console.info function. Function can return true to prevent original function call.
+     * @param {Function} [options.warn] - console.warn function. Function can return true to prevent original function call.
+     * @param {Function} [options.error] - console.error function. Function can return true to prevent original function call.
+	 * @returns {OpenSeadragonImaging.ConsoleHook}
+	 *
+	 **/
+	OSD.Viewer.prototype.addConsoleHook = function (options) {
+		options = options || {};
+		return new $.ConsoleHook(options);
+	};
+
+    /**
+     * Creates a new OpenSeadragonImaging.ConsoleHook.
+     *
+     * @class ConsoleHook
+     * @classdesc Provides hooks into the OpenSeadragon console pipeline.
+     * @memberof OpenSeadragonImaging
+     * @param {Object} options
+     * @param {Function} [options.log] - console.log function. Function can return true to prevent original function call.
+     * @param {Function} [options.debug] - console.debug function. Function can return true to prevent original function call.
+     * @param {Function} [options.info] - console.info function. Function can return true to prevent original function call.
+     * @param {Function} [options.warn] - console.warn function. Function can return true to prevent original function call.
+     * @param {Function} [options.error] - console.error function. Function can return true to prevent original function call.
+     *
+     **/
+    $.ConsoleHook = function (options) {
+        options = options || {};
+
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+				/*jshint loopfunc:true*/
+				// eslint-disable-next-line no-loop-func
+                (function (handler) {
+                    var origHandler = OSD.console[key];
+                    OSD.console[key] = function () {
+                        if (!handler.apply(this, arguments) && origHandler) {
+                            origHandler.apply(this, arguments);
+                        }
+                    };
+                }(options[key]));
+                /*jshint loopfunc:false*/
+            }
+        }
+    };
+
+	/**
+	 * ConsoleHook version.
+	 * @member {Object} OpenSeadragonImaging.ConsoleHook.version
+	 * @property {String} versionStr - The version number as a string ('major.minor.revision').
+	 * @property {Number} major - The major version number.
+	 * @property {Number} minor - The minor version number.
+	 * @property {Number} revision - The revision number.
+	 */
+	$.ConsoleHook.version = {
+		versionStr: '2.0.0'
+	};
+	var versionSplits = $.ConsoleHook.version.versionStr.split('.');
+	$.ConsoleHook.version.major = parseInt(versionSplits[0], 10);
+	$.ConsoleHook.version.minor = parseInt(versionSplits[1], 10);
+	$.ConsoleHook.version.revision = parseInt(versionSplits[2], 10);
+
+
+}(OpenSeadragon, window.OpenSeadragonImaging = window.OpenSeadragonImaging || {}));
---
+++ openseadragon-imaging/tester/scripts/viewmodel.js
@@ -0,0 +1,553 @@
+$(function () {
+	/* global OpenSeadragon,ko */
+
+	var appTitle = 'OpenSeadragon Imaging';
+	var appDesc = 'Test App';
+
+	$(window).resize(onWindowResize);
+
+	var tileSourcesPrefix = './data/';
+	var tileSources = [
+		tileSourcesPrefix + 'testpattern.dzi',
+		tileSourcesPrefix + 'tall.dzi',
+		tileSourcesPrefix + 'wide.dzi',
+		new OpenSeadragon.LegacyTileSource( [{
+			url: tileSourcesPrefix + 'dog_radiograph_2.jpg',
+			width: 1909,
+			height: 1331
+		}] )
+	];
+
+	// var _navExpanderIsCollapsed = true,
+	// 	_$navExpander = $('.navigatorExpander'),
+	// 	_$navExpanderHeaderContainer = $('.expanderHeaderContainer'),
+	// 	_$navExpanderHeader = $(_$navExpanderHeaderContainer.children()[0]),
+	// 	_$navExpanderContentContainer = $('.expanderContentContainer'),
+	// 	_navExpanderExpandedOpacity = 1.0,
+	// 	_navExpanderCollapsedOpacity = 0.40,
+	// 	_navExpanderWidth = 190,
+	// 	_navExpanderHeight = 220,
+	// 	_navExpanderCollapsedWidth = _$navExpanderHeader.outerWidth(),
+	// 	_navExpanderCollapsedHeight = _$navExpanderHeaderContainer.outerHeight();
+
+	var viewer = OpenSeadragon({
+		debugMode: true,
+		id: 'osdContainer',
+		prefixUrl: 'lib/openseadragon/images/',
+		useCanvas: true,
+		autoResize: false, // If false, we have to handle resizing of the viewer
+		// blendTime: 0,
+		// wrapHorizontal: true,
+		// visibilityRatio: 0.1,
+		// minZoomLevel: 0.001,
+		// maxZoomLevel: 10,
+		// zoomPerClick: 1.4,
+		minZoomImageRatio: 0,
+		maxZoomPixelRatio: Infinity,
+		smoothTileEdgesMinZoom: Infinity,
+		//------------------
+		// gestureSettingsMouse: {
+		// 	flickEnabled: true
+		// },
+		// gestureSettingsTouch: {
+		// 	pinchRotate: true
+		// },
+		//------------------
+		showNavigationControl: true,
+		navigationControlAnchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT,
+		// showRotationControl: true,
+		// showFlipControl: true,
+		//------------------
+		showNavigator: false,
+		// navigatorSizeRatio: 0.25,
+		navigatorId: 'navigatorDiv1',
+		navigatorAutoResize: false,
+		//------------------
+		sequenceMode: true,
+		// initialPage: 3,
+		// preserveViewport: true,
+		// preserveOverlays: false,
+		showSequenceControl: true,
+		sequenceControlAnchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT,
+		// showReferenceStrip: true,
+		// referenceStripScroll: 'horizontal',
+		// referenceStripElement: null,
+		// referenceStripHeight: null,
+		// referenceStripWidth: null,
+		// referenceStripPosition: 'BOTTOM_LEFT',
+		// referenceStripSizeRatio: 0.2,
+		//------------------
+		collectionMode: false,
+		// collectionLayout: 'horizontal',
+		collectionRows: 2,
+		collectionColumns: 2,
+		// collectionTileSize: 800,
+		// collectionTileMargin: 80,
+		//------------------
+		tileSources: tileSources
+	});
+
+	viewer.addConsoleHook({
+		// log: function (msg) {
+		// 	return true;
+		// },
+		debug: function (msg) {
+			outputVM.consoleRows.push({
+				msg: msg || '<NOMSG>',
+				itemClass: 'console-item-debug'
+			});
+			var $list = $('.console-list-container');
+			$list.scrollTop($list[0].scrollHeight);
+			return true;
+		},
+		// info: function (msg) {
+		// 	return true;
+		// },
+		// warn: function (msg) {
+		// 	return true;
+		// },
+		error: function (msg) {
+			outputVM.consoleRows.push({
+				msg: msg || '<NOMSG>',
+				itemClass: 'console-item-error'
+			});
+			var $list = $('.console-list-container');
+			$list.scrollTop($list[0].scrollHeight);
+			return true;
+		}
+	});
+
+	// // eslint-disable-line no-unused-vars
+	// var annoHost = viewer.activateAnnoHost({
+	// 	worldIndex: 0,
+	// 	onImageViewChanged: onImageViewChanged
+	// });
+	// // eslint-disable-line no-unused-vars
+	// eslint-disable-line no-unused-vars
+	var annoHost = viewer.activateImagingHelper({
+		worldIndex: 0//,
+		//onImageViewChanged: onImageViewChanged
+	});
+
+	// // eslint-disable-line no-unused-vars
+	// viewer.addViewerInputHook({hooks: [
+	// 	// {tracker: 'viewer', handler: 'moveHandler', hookHandler: onHookOsdViewerMove},
+	// 	{tracker: 'viewer', handler: 'scrollHandler', hookHandler: onHookOsdViewerScroll}//,
+	// 	// {tracker: 'viewer', handler: 'clickHandler', hookHandler: onHookOsdViewerClick}
+	// ]});
+	var _$osdCanvas = null;
+	//var _$annoHostSvg = $('.osdi-annohost-svg');
+
+	// // Workaround for Edge browser where SVG foreignObject elements don't rescale properly via scale transform
+	// //  (Issue: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/4320441/)
+	// void(new MutationObserver(function (muts) {
+	// 	for (var i = muts.length; i--;) {
+	// 		var mut = muts[i], objs = mut.target.querySelectorAll('foreignObject');
+	// 		for (var j = objs.length; j--;) {
+	// 			var obj = objs[j];
+	// 			// var val = obj.style.display;
+	// 			// obj.style.display = 'none';
+	// 			// obj.getBBox();
+	// 			// obj.style.display = val;
+	// 			if (obj.style) {
+	// 				var val = obj.style.visibility;
+	// 				obj.style.visibility = 'hidden';
+	// 				obj.getBBox();
+	// 				obj.style.visibility = val;
+	// 			}
+	// 		}
+	// 	}
+	// }).observe(document.documentElement, {attributes: true, attributeFilter: ['transform'], subtree: true}));
+
+	// //TODO Need workaround for Edge browser where vector-effect="non-scaling-stroke" is not honored
+	// //  (Issue: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/7326047/
+
+	// // // Example SVG annotation overlay.  We use these observables to keep the example annotation sync'd with the image zoom/pan
+	// // var annoGroupTranslateX = ko.observable(0.0),
+	// // 	annoGroupTranslateY = ko.observable(0.0),
+	// // 	annoGroupScale = ko.observable(1.0),
+	// // 	annoGroupTransform = ko.computed(function () {
+	// // 		return 'translate(' + annoGroupTranslateX() + ',' + annoGroupTranslateY() + ') scale(' + annoGroupScale() + ')';
+	// // 		//return 'translate(' + annoGroupTranslateX() + ',' + annoGroupTranslateY() + ')';
+	// // 	}, this);
+
+	viewer.addHandler('open', function (event) {
+		_$osdCanvas = $(viewer.canvas);
+		setMinMaxZoomForImage();
+		vm.haveImage(true);
+		// _$osdCanvas.on('mouseenter.osdimaginghelper', onOsdCanvasMouseEnter);
+		// _$osdCanvas.on('mousemove.osdimaginghelper', onOsdCanvasMouseMove);
+		// _$osdCanvas.on('mouseleave.osdimaginghelper', onOsdCanvasMouseLeave);
+
+		// _$navExpander.css( 'visibility', 'visible');
+		// if (_navExpanderIsCollapsed) {
+		// 	_navExpanderDoCollapse(false);
+		// }
+		// else {
+		// 	_navExpanderDoExpand(true);
+		// }
+
+		//_$annoHostSvg.css( 'visibility', 'visible');
+
+		//// Example OpenSeadragon overlay
+		//var olDiv = document.createElement('div');
+		//olDiv.style.background = 'rgba(255,0,0,0.25)';
+		//var olRect = new OpenSeadragon.Rect(-0.1, -0.1, 1.2, 1.0 / event.viewer.source.aspectRatio + 0.2);
+		////var olRect = new OpenSeadragon.Rect(-0.5, -0.5, 2.0, 1.0 / event.viewer.source.aspectRatio + 1.0);
+		//_this._osd.drawer.addOverlay({
+		//    element: olDiv,
+		//    location: olRect,
+		//    placement: OpenSeadragon.OverlayPlacement.TOP_LEFT
+		//    //onDraw: function (position, size, element) {
+		//    //    position = position || null;
+		//    //}
+		//});
+
+		// // Example OpenSeadragon overlay
+		// // var img = document.createElement('img');
+		// // img.src = 'content/images/openseadragon/next_rest.png';
+		// var canvasElement;
+		// var m_context;
+		// canvasElement = document.createElement('canvas');
+		// canvasElement.width = 200;
+		// canvasElement.height = 200;
+		// m_context = canvasElement.getContext('2d');
+		// m_context.fillStyle = '#FF0000';
+		// m_context.fillRect(4, 4, 192, 192);
+		// //--------------------------------------------------
+		// // //TODO this should be possible...fix in OSD!
+		// // var overlay = new OpenSeadragon.Overlay({
+		// // 	element: canvasElement,
+		// // 	location: new OpenSeadragon.Point(0.02, 0.02),
+		// // 	placement: OpenSeadragon.Placement.TOP_LEFT,
+		// // 	//onDraw: , // OnDrawCallback(position, size, element)
+		// // 	checkResize: false,
+		// // 	//width: ,
+		// // 	//height: ,
+		// // 	rotationMode: OpenSeadragon.OverlayRotationMode.EXACT // NO_ROTATION, EXACT, BOUNDING_BOX
+		// // });
+		// // viewer.addOverlay(overlay);
+		// //--------------------------------------------------
+		// viewer.addOverlay(canvasElement,
+		// 		new OpenSeadragon.Point(0.02, 0.02),
+		// 		OpenSeadragon.Placement.TOP_LEFT);
+
+		// var originalPos;
+		// new OpenSeadragon.MouseTracker({
+		// 	element: canvasElement,
+		// 	pressHandler: function (event) {
+		// 		originalPos = ;
+		// 	},
+		// 	dragHandler: function (event) {
+		// 		// let windowCoords = new OpenSeadragon.Point(event.originalEvent.x, event.originalEvent.y);
+		// 		// let viewportCoords = viewer.viewport.windowToViewportCoordinates(windowCoords);
+		// 		// let overlay = viewer.getOverlayById(this.element);
+
+		// 		// //todo: somehow calculate these values to update the overlay size as the user drags
+		// 		// deltax=0;
+		// 		// deltay=0;
+		// 		// let r = new OpenSeadragon.Rect(viewportCoords.x, viewportCoords.y, overlay.width+deltax, overlay.height+deltax);
+		// 		// overlay.update(r, OpenSeadragon.Placement.CENTER);
+		// 		// overlay.drawHTML(this.element.parentNode, viewer.viewport);
+
+		// 		// console.log("drag delta: " + event.delta + " viewportCoords: " + viewportCoords + "windowCoords: " + windowCoords);
+		// 	}
+		// }).setTracking(true);
+
+		// Example OpenSeadragon overlays
+		// var locPoint = new OpenSeadragon.Point(0.02, 0.02);
+		// var locPointPlus = new OpenSeadragon.Point(0.02, 0.02);
+		// var overlay;
+		// var m_canvas;
+		// var m_context;
+		// for (var x = 0; x < 50; x++) {
+		// 	m_canvas = document.createElement('canvas');
+		// 	m_canvas.width = 200;
+		// 	m_canvas.height = 200;
+		// 	m_context = m_canvas.getContext('2d');
+		// 	m_context.fillStyle = '#FF0000';
+		// 	m_context.fillRect(4, 4, 192, 192);
+		// 	// m_context.moveTo(p1.x, p1.y);
+		// 	// m_context.lineTo(p2.x, p2.y);
+		// 	// m_context.stroke();
+		// 	overlay = new OpenSeadragon.Overlay({
+		// 		element: m_canvas,
+		// 		location: locPoint,
+		// 		placement: OpenSeadragon.Placement.TOP_LEFT,
+		// 		//onDraw: , // OnDrawCallback(position, size, element)
+		// 		checkResize: false,
+		// 		//width: ,
+		// 		//height: ,
+		// 		rotationMode: OpenSeadragon.OverlayRotationMode.EXACT // NO_ROTATION, EXACT, BOUNDING_BOX
+		// 	});
+		// 	viewer.addOverlay(overlay);
+		// 	locPoint = locPoint.plus(locPointPlus);
+		// }
+	});
+
+	viewer.addHandler('close', function (event) {
+		// _$navExpander.css( 'visibility', 'hidden');
+		//_$annoHostSvg.css( 'visibility', 'hidden');
+		vm.haveImage(false);
+		// _$osdCanvas.off('mouseenter.osdimaginghelper', onOsdCanvasMouseEnter);
+		// _$osdCanvas.off('mousemove.osdimaginghelper', onOsdCanvasMouseMove);
+		// _$osdCanvas.off('mouseleave.osdimaginghelper', onOsdCanvasMouseLeave);
+		_$osdCanvas = null;
+	});
+
+	viewer.addHandler('navigator-scroll', function (event) {
+		if (event.scroll > 0) {
+			annoHost.zoomIn();
+		}
+		else {
+			annoHost.zoomOut();
+		}
+	});
+
+	viewer.addHandler('pre-full-page', function (event) {
+		if (event.fullPage) {
+			// Going to full-page mode...remove our bound DOM elements
+			vm.outputVM(null);
+			//vm.svgOverlayVM(null);
+		}
+	});
+
+	viewer.addHandler('full-page', function (event) {
+		if (!event.fullPage) {
+			// Exited full-page mode...restore our bound DOM elements
+			vm.outputVM(outputVM);
+			//vm.svgOverlayVM(svgOverlayVM);
+			//_$annoHostSvg.css( 'visibility', 'visible');
+		}
+	});
+
+	viewer.addHandler('pre-full-screen', function (event) {
+		if (event.fullScreen) {
+			// Going to full-screen mode...remove our bound DOM elements
+			vm.outputVM(null);
+			//vm.svgOverlayVM(null);
+		}
+	});
+
+	viewer.addHandler('full-screen', function (event) {
+		if (!event.fullScreen) {
+			// Exited full-screen mode...restore our bound DOM elements
+			vm.outputVM(outputVM);
+			//vm.svgOverlayVM(svgOverlayVM);
+			//_$annoHostSvg.css( 'visibility', 'visible');
+		}
+	});
+
+	function setMinMaxZoomForImage() {
+		var minzoomX = 50.0 / annoHost.imgWidth;
+		var minzoomY = 50.0 / annoHost.imgHeight;
+		var minZoom = Math.min(minzoomX, minzoomY);
+		var maxZoom = 10.0;
+		annoHost.setMinZoom(minZoom);
+		annoHost.setMaxZoom(maxZoom);
+		annoHost.setZoomStepPercent(35);
+	}
+
+	// function onImageViewChanged(event) {
+	// 	// event.viewportWidth == width of viewer viewport in logical coordinates relative to image native size
+	// 	// event.viewportHeight == height of viewer viewport in logical coordinates relative to image native size
+	// 	// event.viewportOrigin == OpenSeadragon.Point, top-left of the viewer viewport in logical coordinates relative to image
+	// 	// event.viewportCenter == OpenSeadragon.Point, center of the viewer viewport in logical coordinates relative to image
+	// 	// event.zoomFactor == current zoom factor
+
+	// 	// // Example SVG annotation overlay - keep the example annotation sync'd with the image zoom/pan
+	// 	// //var p = viewer.viewport.pixelFromPoint(new OpenSeadragon.Point(0, 0), true);
+	// 	// var p = annoHost.logicalToPhysicalPoint(new OpenSeadragon.Point(0, 0));
+	// 	// annoGroupTranslateX(p.x);
+	// 	// annoGroupTranslateY(p.y);
+	// 	// annoGroupScale(annoHost.getZoomFactor());
+	// }
+
+	// // function onHookOsdViewerMove(event) {
+	// // 	// set event.stopHandlers = true to prevent any more handlers in the chain from being called
+	// // 	// set event.stopBubbling = true to prevent the original event from bubbling
+	// // 	// set event.preventDefaultAction = true to prevent viewer's default action
+	// // 	outputVM.osdMouseRelativeX(event.position.x);
+	// // 	outputVM.osdMouseRelativeY(event.position.y);
+	// // 	event.stopHandlers = true;
+	// // 	event.stopBubbling = true;
+	// // 	event.preventDefaultAction = true;
+	// // }
+
+	// function onHookOsdViewerScroll(event) {
+	// 	// set event.stopHandlers = true to prevent any more handlers in the chain from being called
+	// 	// set event.stopBubbling = true to prevent the original event from bubbling
+	// 	// set event.preventDefaultAction = true to prevent viewer's default action
+	// 	var logPoint = annoHost.physicalToLogicalPoint(event.position);
+	// 	if (event.scroll > 0) {
+	// 		annoHost.zoomInAboutLogicalPoint(logPoint);
+	// 	}
+	// 	else {
+	// 		annoHost.zoomOutAboutLogicalPoint(logPoint);
+	// 	}
+	// 	event.stopBubbling = true;
+	// 	event.preventDefaultAction = true;
+	// }
+
+	// // function onHookOsdViewerClick(event) {
+	// // 	// set event.stopHandlers = true to prevent any more handlers in the chain from being called
+	// // 	// set event.stopBubbling = true to prevent the original event from bubbling
+	// // 	// set event.preventDefaultAction = true to prevent viewer's default action
+	// // 	if (event.quick) {
+	// // 		var logPoint = annoHost.physicalToLogicalPoint(event.position);
+	// // 		if (event.shift) {
+	// // 			annoHost.zoomOutAboutLogicalPoint(logPoint);
+	// // 		}
+	// // 		else {
+	// // 			annoHost.zoomInAboutLogicalPoint(logPoint);
+	// // 		}
+	// // 	}
+	// // 	event.stopBubbling = true;
+	// // 	event.preventDefaultAction = true;
+	// // }
+
+	// // function onOsdCanvasMouseEnter(event) {
+	// // 	outputVM.haveMouse(true);
+	// // }
+
+	// // function onOsdCanvasMouseMove(event) {
+	// // 	var osdmouse = OpenSeadragon.getMousePosition(event),
+	// // 		osdoffset = OpenSeadragon.getElementOffset(viewer.canvas);
+	// // 	outputVM.osdMousePositionX(osdmouse.x);
+	// // 	outputVM.osdMousePositionY(osdmouse.y);
+	// // 	outputVM.osdElementOffsetX(osdoffset.x);
+	// // 	outputVM.osdElementOffsetY(osdoffset.y);
+
+	// // 	var offset = _$osdCanvas.offset();
+	// // 	outputVM.mousePositionX(event.pageX);
+	// // 	outputVM.mousePositionY(event.pageY);
+	// // 	outputVM.elementOffsetX(offset.left);
+	// // 	outputVM.elementOffsetY(offset.top);
+	// // 	outputVM.mouseRelativeX(event.pageX - offset.left);
+	// // 	outputVM.mouseRelativeY(event.pageY - offset.top);
+	// // }
+
+	// // function onOsdCanvasMouseLeave(event) {
+	// // 	outputVM.haveMouse(false);
+	// // }
+
+	function onWindowResize() {
+		if (viewer && annoHost && !viewer.autoResize) {
+			// We're handling viewer resizing ourselves. Let the ImagingHelper do it.
+			annoHost.notifyResize();
+		}
+	}
+
+	// _$navExpanderHeaderContainer.on('click', null, function (event) {
+	// 	if (_navExpanderIsCollapsed) {
+	// 		_navExpanderExpand();
+	// 	}
+	// 	else {
+	// 		_navExpanderCollapse();
+	// 	}
+	// });
+
+	// function _navExpanderMakeResizable() {
+	// 	_$navExpander.resizable({
+	// 		disabled: false,
+	// 		handles: 'e, s, se',
+	// 		minWidth: 100,
+	// 		minHeight: 100,
+	// 		maxWidth: null,
+	// 		maxHeight: null,
+	// 		containment: '#osdViewerPane',
+	// 		resize: function (event, ui) {
+	// 			_navExpanderWidth = ui.size.width;
+	// 			_navExpanderHeight = ui.size.height;
+	// 			_navExpanderResizeContent();
+	// 		}
+	// 	});
+	// }
+
+	// function _navExpanderRemoveResizable() {
+	// 	_$navExpander.resizable('destroy');
+	// }
+
+	// function _navExpanderDoExpand(adjustresizable) {
+	// 	if (adjustresizable) {
+	// 		_navExpanderMakeResizable();
+	// 	}
+	// 	_$navExpander.width(_navExpanderWidth);
+	// 	_$navExpander.height(_navExpanderHeight);
+	// 	_$navExpanderContentContainer.show('fast', function () {
+	// 		_navExpanderResizeContent();
+	// 	});
+	// 	_$navExpander.css('opacity', _navExpanderExpandedOpacity);
+	// }
+
+	// function _navExpanderDoCollapse(adjustresizable) {
+	// 	_$navExpander.css('opacity', _navExpanderCollapsedOpacity);
+	// 	_$navExpanderContentContainer.hide('fast');
+	// 	_$navExpander.width(_navExpanderCollapsedWidth);
+	// 	_$navExpander.height(_navExpanderCollapsedHeight);
+	// 	_navExpanderResizeContent();
+	// 	if (adjustresizable) {
+	// 		_navExpanderRemoveResizable();
+	// 	}
+	// }
+
+	// function _navExpanderExpand() {
+	// 	if (_navExpanderIsCollapsed) {
+	// 		_navExpanderDoExpand(true);
+	// 		_navExpanderIsCollapsed = false;
+	// 	}
+	// }
+
+	// function _navExpanderCollapse() {
+	// 	if (!_navExpanderIsCollapsed) {
+	// 		_navExpanderDoCollapse(true);
+	// 		_navExpanderIsCollapsed = true;
+	// 	}
+	// }
+
+	// function _navExpanderResizeContent() {
+	// 	var wrapperwidth = _$navExpander.innerWidth();
+	// 	var wrapperheight = _$navExpander.innerHeight();
+	// 	var headerheight = _$navExpanderHeaderContainer ? _$navExpanderHeaderContainer.outerHeight(true) : 0;
+	// 	var newheight = wrapperheight - headerheight;
+	// 	_$navExpanderContentContainer.width(wrapperwidth);
+	// 	_$navExpanderContentContainer.height(newheight);
+	// 	viewer.navigator.updateSize();
+	// 	viewer.navigator.update(viewer.viewport);
+	// }
+
+	var outputVM = {
+		consoleRows: ko.observableArray([])
+	};
+
+	// var svgOverlayVM = {
+	// 	annoGroupTransform: annoGroupTransform
+	// };
+
+	var vm = {
+		appTitle: ko.observable(appTitle),
+		appDesc: ko.observable(appDesc),
+
+		haveImage: ko.observable(false),
+
+		//svgOverlayVM: ko.observable(svgOverlayVM),
+
+		outputVM: ko.observable(outputVM),
+
+        clearConsoleList: function () {
+			$('.console-list').empty();
+		},
+
+		setCursorModeHand: function () {
+		},
+
+		setCursorModeFinger: function () {
+		}
+	};
+
+	ko.applyBindings(vm);
+
+	$(window).resize();
+
+	vm.setCursorModeHand();
+});
